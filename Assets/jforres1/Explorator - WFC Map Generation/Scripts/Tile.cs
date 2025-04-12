29f2e2391e22c459b8ff409cb7aac1b40587c39c
jforres1 <jforres1@unca.edu>
2025-04-12 11:40:05 -0400

    Folder Reconfiguring for Publishing

---
+++ Assets/jforres1/Explorator - WFC Map Generation/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Explorator
+{
+    public class Tile : MonoBehaviour
+    {
+        public string[] tags;
+        public string[] northCompatibleTags;
+        public string[] southCompatibleTags;
+        public string[] eastCompatibleTags;
+        public string[] westCompatibleTags;
+    }
+
+}
